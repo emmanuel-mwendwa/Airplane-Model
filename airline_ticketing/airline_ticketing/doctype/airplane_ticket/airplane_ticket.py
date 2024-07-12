a42449dd998a149f7e4a7ed25fb5a12739cb0ded
emmanuel-mwendwa <mwendwae054@gmail.com>
2024-07-12 11:17:36 +0300

    Remove duplicate add_ons

--- airline_ticketing/airline_ticketing/doctype/airplane_ticket/airplane_ticket.py
+++ airline_ticketing/airline_ticketing/doctype/airplane_ticket/airplane_ticket.py
@@ -1,13 +1,18 @@
 # Copyright (c) 2024, Mwendwa and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
-
 class AirplaneTicket(Document):
-	
+    
     def validate(self):
+
+        self.remove_duplicate_add_ons()
+        self.calculate_total_amount()
+        
+    def calculate_total_amount(self):
 
         self.total_amount = self.flight_price
 
@@ -17,4 +22,20 @@ class AirplaneTicket(Document):
 
         self.total_amount = round(self.total_amount, 2)
 
-        
+    def remove_duplicate_add_ons(self):
+
+        seen_add_ons = set()
+        unique_add_ons = []
+
+        for add_on in self.add_ons:
+
+            if add_on.item not in seen_add_ons:
+                seen_add_ons.add(add_on.item)
+                unique_add_ons.append(add_on)
+
+            else:
+                frappe.msgprint(
+                    _("Duplicate add-on of type {0} removed.").format(add_on.item),
+                    alert=True
+                )
+        self.add_ons = unique_add_ons
