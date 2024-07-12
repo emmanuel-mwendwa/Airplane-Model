6c491aee6a188af05cc94e0e0df70cf93040c066
emmanuel-mwendwa <mwendwae054@gmail.com>
2024-07-12 10:12:33 +0300

    Total Amount = Flight Price + Sum of amounts of all the add-ons

--- airline_ticketing/airline_ticketing/doctype/airplane_ticket/airplane_ticket.py
+++ airline_ticketing/airline_ticketing/doctype/airplane_ticket/airplane_ticket.py
@@ -6,4 +6,15 @@ from frappe.model.document import Document
 
 
 class AirplaneTicket(Document):
-	pass
+	
+    def validate(self):
+
+        self.total_amount = self.flight_price
+
+        if self.add_ons:
+            for add_on in self.add_ons:
+                self.total_amount += add_on.amount
+
+        self.total_amount = round(self.total_amount, 2)
+
+        
