3cef3e5eb84efe53a8eee8d76dcd8052552a0c7c
emmanuel-mwendwa <mwendwae054@gmail.com>
2024-11-05 14:17:14 +0300

    feat: dialog box to assign seat number

--- airline_ticketing/airline_ticketing/doctype/airplane_ticket/airplane_ticket.js
+++ airline_ticketing/airline_ticketing/doctype/airplane_ticket/airplane_ticket.js
@@ -1,8 +1,27 @@
 // Copyright (c) 2024, Mwendwa and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airplane Ticket", {
-// 	refresh(frm) {
+frappe.ui.form.on("Airplane Ticket", {
+	refresh(frm) {
+        frm.add_custom_button(__("Assign Seat"), function(){
+            const dialog = new frappe.ui.Dialog({
+                title: __("Select Seat"),
+                fields:[
+                    {
+                        label: 'Seat Number',
+                        fieldname: 'seat_number',
+                        fieldtype: 'Data'
+                    }
+                ],
+                size: 'small',
+                primary_action_label: 'Assign',
+                primary_action(values) {
+                    frm.set_value('seat', values.seat_number);
+                    dialog.hide();
+                }
+            });
 
-// 	},
-// });
+            dialog.show();
+        }, __("Actions"));
+	},
+});
