0f3b15f41e7415d8778e2d569b800a16d3c79ab2
emmanuel-mwendwa <mwendwae054@gmail.com>
2024-11-05 13:56:31 +0300

    feat: website link

--- airline_ticketing/airline_ticketing/doctype/airline/airline.js
+++ airline_ticketing/airline_ticketing/doctype/airline/airline.js
@@ -1,8 +1,11 @@
 // Copyright (c) 2024, Mwendwa and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airline", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Airline", {
+	refresh(frm) {
+        const airline_website_link = frm.doc.website;
+        if (frm.doc.website) {
+            frm.add_web_link(airline_website_link, "Visit Website");
+        }
+	},
+});
