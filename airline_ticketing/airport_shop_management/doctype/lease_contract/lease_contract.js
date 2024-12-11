7a4ec6ea5e6e376688768cdfb045645eaffd8491
emmanuel-mwendwa <mwendwae054@gmail.com>
2024-12-11 18:42:04 +0300

    (feat): validate lease time and start and end dates

--- airline_ticketing/airport_shop_management/doctype/lease_contract/lease_contract.js
+++ airline_ticketing/airport_shop_management/doctype/lease_contract/lease_contract.js
@@ -14,6 +14,32 @@ frappe.ui.form.on("Lease Contract", {
                 };
             }
         )
-
 	},
+
+    start_date(frm) {
+        validate_lease_duration(frm);
+    },
+
+    end_date(frm) {
+        validate_lease_duration(frm);
+    }
 });
+
+function validate_lease_duration(frm) {
+    if (frm.doc.start_date && frm.doc.end_date) {
+        const start_date = frappe.datetime.str_to_obj(frm.doc.start_date);
+        const end_date = frappe.datetime.str_to_obj(frm.doc.end_date);
+
+        if (start_date >= end_date) {
+            frappe.msgprint(__("Lease Start Date must be before Lease End Date"));
+            frm.set_value("end_date", null);
+            return;
+        }
+
+        const difference_in_days = frappe.datetime.get_day_diff(end_date, start_date);
+        if (difference_in_days < 30) {
+            frappe.msgprint(__("The lease duration must be at least one month"));
+            frm.set_value("end_date", null);
+        }
+    }
+}
